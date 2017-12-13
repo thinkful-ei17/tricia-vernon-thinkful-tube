e83f76a0f0bd4c4275f023b00228bf657fe5021b
Vernon Mensah <vernonmensah@gmail.com>
2017-12-13 11:14:28 -0500

    A working ajax function and renderResult

--- index.js
+++ index.js
@@ -21,18 +21,33 @@ function getDataFromApi(searchTerm, callback) {
 }
 
 function renderResult(result) {
+  // return `
+  //   <div>
+  //     <h2>
+  //     <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+  //     <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
+  //     <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+  //   </div>
+  // `;
+
   return `
     <div>
       <h2>
-      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
-      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
-      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+      <a class="js-result-name">${result.snippet.title}</a></h2>
+      <p>Description: <span class="js-watchers-count">${result.snippet.description}</span></p>
+      <p>Channel Title: <span class="js-issues-count">${result.snippet["channelTitle"]}</span></p>
     </div>
   `;
 }
 
 function displayGitHubSearchData(data) {
   const results = data.items.map((item, index) => renderResult(item));
+  //example of items sent out from data
+  console.log(data.items[0]);
+  console.log(data.items[0].snippet.title);
+  console.log(data.items[0].snippet.channelId);
+  console.log("~~~~~~~~");
+  console.log(results);
   $('.js-search-results').html(results);
 }
 
@@ -42,7 +57,7 @@ function watchSubmit() {
     const queryTarget = $(event.currentTarget).find('.js-query');
     const query = queryTarget.val();
     // clear out the input
-    queryTarget.val("");
+    // queryTarget.val("");
     getDataFromApi(query, displayGitHubSearchData);
   });
 }

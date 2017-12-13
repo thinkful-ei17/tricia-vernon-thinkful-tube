58dc308964b8224e392ad6fd8698574aea74c88f
TriciaF <TriciaF@thinkful.com>
2017-12-13 12:11:08 -0500

    working on url thumbnail

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-const youtubeKey = "AIzaSyCAq3BR_9P-dfHlFxLUoTm_tf570UyzfyA";
+const youtubeKey = 'AIzaSyCAq3BR_9P-dfHlFxLUoTm_tf570UyzfyA';
 
 function getDataFromApi(searchTerm, callback) {
   const settings = {
     url: YOUTUBE_SEARCH_URL,
     data: {
-      part: "snippet",
+      part: 'snippet',
       q: `${searchTerm}`,
-      type: "video",
+      type: 'video',
       key: youtubeKey
     },
     dataType: 'json',
@@ -24,7 +24,7 @@ function renderResult(result) {
   // return `
   //   <div>
   //     <h2>
-  //     <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+  //     <a class="js-result-name" href="${result.snippet.title}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
   //     <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
   //     <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
   //   </div>
@@ -35,10 +35,13 @@ function renderResult(result) {
       <h2>
       <a class="js-result-name">${result.snippet.title}</a></h2>
       <p>Description: <span class="js-watchers-count">${result.snippet.description}</span></p>
-      <p>Channel Title: <span class="js-issues-count">${result.snippet["channelTitle"]}</span></p>
+      <p>Channel Title: <span class="js-issues-count">${result.snippet['channelTitle']}</span></p>
+      
     </div>
   `;
 }
+
+{ /* <img src='${result.snippet.[default].url}'></img> */ }
 
 function displayGitHubSearchData(data) {
   const results = data.items.map((item, index) => renderResult(item));
@@ -46,7 +49,8 @@ function displayGitHubSearchData(data) {
   console.log(data.items[0]);
   console.log(data.items[0].snippet.title);
   console.log(data.items[0].snippet.channelId);
-  console.log("~~~~~~~~");
+  console.log(data.items[0].snippet['default']);
+  console.log('~~~~~~~~');
   console.log(results);
   $('.js-search-results').html(results);
 }

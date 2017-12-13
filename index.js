ad12f2035a3687d3d318de9d2dbc698a36b8a2f3
TriciaF <TriciaF@thinkful.com>
2017-12-13 15:37:38 -0500

    added next and prev button functionality

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+'use strict';
+/*global $*/
+
 const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 const youtubeKey = 'AIzaSyCAq3BR_9P-dfHlFxLUoTm_tf570UyzfyA';
+const STORE = {
+  next: '',
+  prev: '',
+  searchTerm: ''
+};
 
 function getDataFromApi(searchTerm, callback) {
   const settings = {
@@ -15,6 +23,43 @@ function getDataFromApi(searchTerm, callback) {
     dataType: 'json',
     type: 'GET',
     success: callback
+  };
+
+  $.ajax(settings);
+}
+
+function getNextDataFromApi(searchTerm, nextPageToken, callBack) {
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    data: {
+      part: 'snippet',
+      q: `${searchTerm}`,
+      type: 'video',
+      key: youtubeKey,
+      pageToken: nextPageToken
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callBack,
+
+  };
+
+  $.ajax(settings);
+}
+
+function getPrevDatafromApi(searchTerm, prevPageToken, callBack) {
+  const settings = {
+    url: YOUTUBE_SEARCH_URL,
+    data: {
+      part: 'snippet',
+      q: `${searchTerm}`,
+      type: 'video',
+      key: youtubeKey,
+      pageToken: prevPageToken
+    },
+    dataType: 'json',
+    type: 'GET',
+    success: callBack
   };
 
   $.ajax(settings);
@@ -35,13 +80,35 @@ function renderResult(result) {
       <h2>
       <a class="js-result-name">${result.snippet.title}</a></h2>
       <p>Description: <span class="js-watchers-count">${result.snippet.description}</span></p>
-      <p>Channel Title: <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank"><span class="js-issues-count">${result.snippet.channelTitle}</span></p></a>
+      <p>Channel Title: <a href="https://www.youtube.com/channel/${result.snippet.channelId}" target="_blank"><span class="js-issues-count">${result.snippet.channelTitle}</span></p>
+      </a>
       <a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img src="${result.snippet.thumbnails.high.url}"></img></a>
     </div>
   `;
 }
 
-{ /* <img src='${result.snippet.[default].url}'></img> */ }
+function handleNextPage() {
+  $('.next').on('click', event => {
+    console.log('handleNextPage');
+    if (STORE.next) {
+      getNextDataFromApi(STORE.searchTerm, STORE.next, displayGitHubSearchData);
+    }
+  });
+}
+
+function handlePreviousPage() {
+  $('.prev').on('click', event => {
+    console.log('handlePrevPage');
+    if (STORE.prev) {
+      getPrevDatafromApi(STORE.searchTerm, STORE.prev, displayGitHubSearchData);
+    }
+  });
+}
+
+
+function displayPrevDispayNextData(data) {
+  //
+}
 
 function displayGitHubSearchData(data) {
   const results = data.items.map((item, index) => renderResult(item));
@@ -50,14 +117,27 @@ function displayGitHubSearchData(data) {
   console.log(data.items[0]);
   console.log(data.items[0].snippet.title);
   console.log(data.items[0].snippet.channelId);
-  console.log("TITLE:");
+  console.log('TITLE:');
   console.log(data.items[0].snippet.channelTitle);
-  console.log("!!!!!!!!");
+  console.log('!!!!!!!!');
   console.log(data.items[0].snippet.thumbnails['default']);
   console.log('~~~~~~~~');
   console.log(data.items[0].snippet.thumbnails.default);
+  console.log('NEXT and PREV Token');
+  console.log(data.nextPageToken);
+  console.log(data.prevPageToken);
   console.log(results);
+
+  if (data.nextPageToken) {
+    STORE.next = data.nextPageToken;
+    console.log(STORE.next);
+  }
+  if (data.prevPageToken) {
+    STORE.prev = data.prevPageToken;
+    console.log(STORE.prev);
+  }
   $('.js-search-results').html(results);
+
 }
 
 function watchSubmit() {
@@ -68,7 +148,10 @@ function watchSubmit() {
     // clear out the input
     // queryTarget.val("");
     getDataFromApi(query, displayGitHubSearchData);
+    STORE.searchTerm = query;
   });
 }
 
 $(watchSubmit);
+$(handleNextPage);
+$(handlePreviousPage);
